60b42c1943ac38446d0b9fc5beee0a51f65f6b58
Your Name <you@example.com>
2021-10-22 22:26:22 -0400

    x

--- sed02.sh
+++ sed02.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
 toptop=spicy_202003
-dirdir=docs/
 
-[ 33 = 34 ] && sed -i \
-    -e "s;\bcontent=/;&${toptop};g" \
-    -e "s;\bhref=/;&${toptop};g" \
-    -e "s;\bsrc=/;&${toptop};g" \
-    -e "s;\b${toptop}/${toptop};${toptop};g" \
-    -e "s;\b${toptop}/${toptop};${toptop};g" \
-    `find ${dirdir} -type f \( -name "*.html" -o -name "*.css" \) `
-
-( 
-cd docs && \
-    rm -f   ${toptop} ; \
-    ln -s . ${toptop} \
-    )
+[ -d docs ] \
+    && mkdir   docs/${toptop}  \
+    && (mv docs/* docs/${toptop}/ 2>/dev/null ; cp docs/${toptop}/index.html docs/)
 
